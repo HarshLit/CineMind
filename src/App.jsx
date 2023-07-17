086598b57b146402d41edf12e2af862229134c3a
Nikhil Sharma <niks3319@gmail.com>
2023-07-17 15:57:41 +0530

    404 handling

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Footer from "./components/footer/Footer"
 import Details from "./pages/details/Details"
 import Explore from "./pages/explore/Explore"
 import Home from "./pages/home/Home"
-import PageNotFound from './pages/404/pageNotFound';
+import PageNotFound from './pages/404/PageNotFound';
 import SearchResult from './pages/searchResult/SearchResult';
 import Person from "./pages/person/Person"
 
