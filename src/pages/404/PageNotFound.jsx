38172562c36955c663fa862192d2ed74ac00f52d
Nikhil Sharma <niks3319@gmail.com>
2023-07-17 15:55:26 +0530

    404 handling

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import "./style.scss"
+import "./style.scss";
+
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 
 const PageNotFound = () => {
   return (
-    <div>
-      
-    </div>
+    <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
   )
 }
 
