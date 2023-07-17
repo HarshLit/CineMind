694d13240a53cc071bd28dd22df515e99ab60c9a
Nikhil Sharma <niks3319@gmail.com>
2023-07-17 16:00:12 +0530

    error handling resolve

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -9,7 +9,7 @@ import useFetch from "../../hooks/useFetch";
 import { fetchDataFromAPI } from "../../utils/api";
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 import MovieCard from "../../components/movieCard/MovieCard";
-import Spinner from "../../components/spinner/Spinner";
+import Spinner from "../../components/Spinner/Spinner";
 
 let filters = {};
 
